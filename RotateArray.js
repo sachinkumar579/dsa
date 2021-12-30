d8bc5bd7c5a6e5fa4e7535517f11c310f511feee
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-12-30 11:45:25 +0530

    Update RotateArray.js

--- RotateArray.js
+++ RotateArray.js
@@ -1,21 +1,15 @@
 // https://leetcode.com/problems/rotate-array/
 
 var rotate = function(nums, k) {
-    
-     k = k%nums.leng 
+  
+   k = k % nums.length 
 
-     reverse(0,nums,nums.length/2,nums.length-1)
-     reverse(0,nums,Math.floor(k/2),k-1)
-     reverse(k,nums,(k+nums.length)/2,k+nums.length-1)
+   let resultArr = [] 
 
-     function reverse(start,nums,length,counter){ 
-     for (let index = start; index < length; index++) {
-       let temp = nums[counter-index]
-       nums[counter-index] = nums[index]
-       nums[index] = temp  
-     }
-    }
-   console.log(nums)
+   for (let index = 0; index < nums.length; index++) {
+           resultArr[(index+k)%nums.length] = nums[index]
+   }
+
 };
 
-console.log(rotate([1,2,3,4,5],7))
+console.log(rotate([1,2,3,4,5,6],6))
